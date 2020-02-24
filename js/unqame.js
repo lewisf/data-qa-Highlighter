1c6a3dc8c2eb646f28611ed44851eeeadfc388db
Lewis Chung <lewis@withpersona.com>
2020-02-24 10:28:37 -0800

    Use #id, and data-react-class instead

--- js/unqame.js
+++ js/unqame.js
@@ -1,29 +1,28 @@
 // Removing highlighters and rolling back the magic.
 var qaboxelem = document.querySelectorAll(".qa-box");
-var qaelem = '[data-qa], [data-test]';
+var qaelem = "#id, [data-react-class]";
 // DIV selector
 var divs = document.getElementsByTagName("DIV");
 // QA elements selector
 var dataqas = document.querySelectorAll(qaelem);
 
 for (var i = 0; i < dataqas.length; i++) {
-    var d = dataqas[i];
-    d.style.border = "";
-    d.style.backgroundColor = "";
-    d.style.position = "";
-    d.parentElement.style.position = "";
-    d.classList.remove("qa-bg");
+  var d = dataqas[i];
+  d.style.border = "";
+  d.style.backgroundColor = "";
+  d.style.position = "";
+  d.parentElement.style.position = "";
+  d.classList.remove("qa-bg");
 }
 
 for (var i = 0; i < qaboxelem.length; i++) {
-    qaboxelem[i].parentNode.removeChild(qaboxelem[i]);
+  qaboxelem[i].parentNode.removeChild(qaboxelem[i]);
 }
-
 
 for (var i = 0; i < divs.length; i++) {
-    divs[i].style.overflow = "visible";
+  divs[i].style.overflow = "visible";
 }
 
-[].forEach.call(qaboxelem, function (el) {
-    el.classList.remove("qa-box");
+[].forEach.call(qaboxelem, function(el) {
+  el.classList.remove("qa-box");
 });
