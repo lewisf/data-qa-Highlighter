6a40e0787a52a644fa34065eed783dd973d2648b
Lewis Chung <lewis@withpersona.com>
2020-05-31 15:47:53 -0700

    fix: 🐛 Shrink font size

--- js/qame.js
+++ js/qame.js
@@ -14,15 +14,15 @@ for (var i = 0; i < dataqas.length; i++) {
   }
   var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   var qabox =
-    '<div class="qa-box" style="z-index:100000;opacity:0.9;position:absolute;font-family:monospace;">' +
-    '<div style="position:relative;background-color:' +
+    '<div class="qa-box" style="z-index:100000;opacity:0.8;position:absolute;font-family:monospace;">' +
+    '<div class="qa-text" style="font-size:0.8rem;position:relative;background-color:' +
     randomColor +
     '">' +
     qaBoxTxt +
     "</div>" +
     "</div>";
   var elems = ["SELECT", "TEXTAREA", "INPUT", "BUTTON", "SPAN"];
-  d.style.border = "2px inset " + randomColor;
+  d.style.border = "2px solid " + randomColor;
   // d.style.position = "absolute";
   // d.parentElement.style.position = "relative";
   d.className += " qa-bg";
